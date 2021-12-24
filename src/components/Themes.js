edc165a14988e73f1149a30689199f76fd525b80
AjithArun2001 <ajitharu201@gmail.com>
2021-12-24 18:35:22 +0530

    hacksus1.5.2

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,38 +1,10 @@
-import React,{useState, useEffect} from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
 
-
-function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  }
-  
-  function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(
-      getWindowDimensions()
-    );
-  
-    useEffect(() => {
-      function handleResize() {
-        setWindowDimensions(getWindowDimensions());
-      }
-  
-      window.addEventListener("resize", handleResize);
-      return () => window.removeEventListener("resize", handleResize);
-    }, []);
-  
-    return windowDimensions;
-  }
-
 function Themes() {
 
-    const { height, width } = useWindowDimensions();
 
-    console.log(height);
 
 
     const TracksBox = styled.div`
@@ -56,45 +28,11 @@ function Themes() {
         }
     `
 
-    const [cardFlip, setCardFlip] = useState([true,true,true,true,true,true,true,true,true,true]);
 
-    /* const handleClick = (x,id) => {
-        if(width <= 1200){
-            const newArr = [...cardFlip];
-            newArr[id-1] = x;
-            setCardFlip(newArr);
-        }
-    }
-
-    const handleHover = (x,id) => {
-        if(width > 1200){
-            const newArr = [...cardFlip];
-            newArr[id-1] = x;
-            setCardFlip(newArr);
-        }
-
-    } */
-
-    /* width <= 1200 ? onClick={() => handleClick(false, id)} : onHover={() => handleClick(false, id)} */
 
     return (
         <>
-        {/* <Container id="themes">
-            <h1>Themes</h1>
-            <ThemesBox>
-                {Theme.map(({id,theme,info}) => 
-                    cardFlip[id-1] ?
-                        <div onClick={() => handleClick(false, id)} onMouseEnter={() => handleHover(false,id)} id={id}>
-                            <span>{id}</span>
-                            <p>{theme}</p>
-                        </div>
-                    :
-                        <InfoDiv onClick={() => handleClick(true, id)} onMouseLeave={() => handleHover(true,id)}>
-                            <p>{info}</p>
-                        </InfoDiv>
-                )}
-            </ThemesBox>
-        </Container> */}
+ 
         <TracksBox id="tracks">
             <h1>Tracks</h1>
             <div>Hardware</div>
