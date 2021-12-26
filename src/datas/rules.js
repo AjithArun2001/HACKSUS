2d52ce667b76e9e4e53f74dd4232eecf264de62f
AjithArun2001 <ajitharu201@gmail.com>
2021-12-26 09:37:13 +0530

    changes

--- src/datas/rules.js
+++ src/datas/rules.js
@@ -7,7 +7,7 @@ const rules = [
     {id:6, rule: "Adhering to the rules and guidelines throughout the hackathon is of utmost importance and any violation will be dealt seriously and can lead to disqualification."},
     {id:7, rule: "Participants are expected to co-operate with the volunteers throughout the hackathon."},
     {id:8, rule: "Participants are not allowed to leave the college premises at any circumstances during the hackathon. In case of emergencies, the participant may contact the event coordinator for consideration."},
-    {id:9, rule: "Food and accommodation for the participants have been arranged for the 36 hour duration of the hackathon. Food tokens will be made available for the same which must be carried throughout the event."},
+    {id:9, rule: "Food and nap facility for the participants have been arranged for the 36 hour duration of the hackathon. Food tokens will be made available for the same which must be carried throughout the event."},
     {id:10, rule: "The ID card provided by the college must be carried during the hackathon."},
     {id:11, rule: "Use of abusive and vulgar language will be taken seriously and can lead to disqualification. Remember that harassment and racist, sexist, or exclusionary jokes are not appropriate for this event. If at any point you see a fellow hacker being harassed, please talk to the nearest hackathon volunteer."},
     {id:12, rule: "Vandalism of college properties, premises and resources will incur penalties and even disqualification from the event."},
